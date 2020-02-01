ec40040b9cfb4abd051ddd1c0ab37e19361006b6
山崎駿 <>
2020-02-01 20:25:51 +0900

    Titan NavMesh Move

--- Assets/Script/Enemys/PerceptionRange.cs
+++ Assets/Script/Enemys/PerceptionRange.cs
@@ -12,26 +12,28 @@ public class PerceptionRange : MonoBehaviour
 
     void OnTriggerStay(Collider other)
     {
-        //// コライダーでプレイヤーを検知
-        //if(other.CompareTag("Player"))
-        //{
-        //    if (Physics.Raycast(transform.position, (other.transform.position - transform.position), out RaycastHit raycastHit))
-        //    {
-        //        // Rayが当たっている間プレイヤーを捕捉
-        //        if (raycastHit.transform.CompareTag("Player"))
-        //        {
-        //            enemy.FoundPlayer(other.transform.position);
-        //        }
-        //        else
-        //        {
-        //            enemy.LostPlayer(other.transform);
-        //        }
-        //    }
-        //    else
-        //    {
-        //        enemy.LostPlayer(other.transform);
-        //    }
-        //}
+        // コライダーでプレイヤーを検知
+        if(other.CompareTag("Player"))
+        {
+            Debug.DrawRay(transform.position, (other.transform.position - transform.position));
+
+            //if (Physics.Raycast(transform.position, (other.transform.position - transform.position), out RaycastHit raycastHit))
+            //{
+            //    // Rayが当たっている間プレイヤーを捕捉
+            //    if (raycastHit.transform.CompareTag("Player"))
+            //    {
+            //        enemy.FoundPlayer(other.transform.position);
+            //    }
+            //    else
+            //    {
+            //        enemy.LostPlayer(other.transform);
+            //    }
+            //}
+            //else
+            //{
+            //    enemy.LostPlayer(other.transform);
+            //}
+        }
         //else
         //{
         //    //enemy.LostPlayer(other.transform);
