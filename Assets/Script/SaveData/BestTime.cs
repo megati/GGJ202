2da175cb859305407ffdc24fd59fde0316e58f65
山崎駿 <>
2020-02-02 00:16:18 +0900

    jsonにベスト記録を保存、読み書きができる準備

---
+++ Assets/Script/SaveData/BestTime.cs
@@ -0,0 +1,8 @@
+﻿/// <summary>
+/// 最高記録
+/// </summary>
+[System.Serializable]
+public struct BestTime
+{
+    public int bestRecord;
+}
---
+++ Assets/Script/SaveData/DataSave.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DataSave : MonoBehaviour
+{
+    /// <summary>
+    /// ローカルデータを保存するファイルのパス
+    /// </summary>
+    string _localDataPath => Path.Combine(Application.dataPath, "LocalData");
+
+    /// <summary>
+    /// データを 暗号化・JSON化 し保存する
+    /// </summary>
+    /// <typeparam name="T">Serializableなクラス</typeparam>
+    /// <param name="data"></param>
+    public void SaveLocalDataToJson(BestTime data)
+    {
+        if (!Directory.Exists(_localDataPath))
+        {
+            Directory.CreateDirectory(_localDataPath);
+        }
+
+        using (var streamWriter = new StreamWriter(Path.Combine(_localDataPath, typeof(BestTime).Name)))
+        {
+            var text = JsonUtility.ToJson(data);
+            text = AvoEx.AesEncryptor.Encrypt(text);
+            streamWriter.Write(text);
+        }
+    }
+
+    /// <summary>
+    /// 複合化されたJSONデータを取得
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    public BestTime LoadLocalData()
+    {
+        if (!File.Exists(Path.Combine(_localDataPath, typeof(BestTime).Name)))
+        {
+            SaveLocalDataToJson(new BestTime());
+        }
+
+        using (var streamReader = new StreamReader(Path.Combine(_localDataPath, typeof(BestTime).Name)))
+        {
+            var json = streamReader.ReadToEnd();
+            json = AvoEx.AesEncryptor.DecryptString(json);
+            return JsonUtility.FromJson<BestTime>(json);
+        }
+    }
+}
