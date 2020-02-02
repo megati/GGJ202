24de3c6ef3e4b2e2694bbac97e8c070bf5d60f26
namiton <namito373@gmail.com>
2020-02-02 13:16:55 +0900

    タイトル(みかん)

--- Assets/Script/SaveData/DataSave.cs
+++ Assets/Script/SaveData/DataSave.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class DataSave : MonoBehaviour
+public class DataSave 
 {
     /// <summary>
     /// ローカルデータを保存するファイルのパス
---
+++ Assets/Script/UI/Title/TitleManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text Timetext=null;
+
+    // Update is called once per frame
+    private void Awake()
+    {
+        var bestTime = new DataSave().LoadLocalData();
+        bestTime.bestRecord = 200;
+        var sec = bestTime.bestRecord % 60;
+        var min = bestTime.bestRecord / 60;
+        Timetext.text = "BEST TIME: "+min.ToString()+":"+sec;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+        if(Input.GetMouseButtonDown(1))
+        {
+
+        }
+    }
+}
