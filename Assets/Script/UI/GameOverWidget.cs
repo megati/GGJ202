b4b8b7053f38d25ba1ec54cf7ee0506fb920861f
山崎駿 <>
2020-02-02 10:05:03 +0900

    Create GameOverWidget

---
+++ Assets/Script/UI/GameOverWidget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverWidget : MonoBehaviour
+{
+    void Update()
+    {
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            SceneController.Instance.LoadSceneAsync(SceneName.MasterGame);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+
+        }
+    }
+}
